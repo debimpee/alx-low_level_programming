c2372043f8e91fb3dc4ec325091527a74f4825e1
debimpee <adebimpefolayemi1@gmail.com>
2022-06-06 11:43:41 -0700

    dod

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char m = '0';
+
+	for (m = '0'; m <= '9'; m++)
+	{
+	putchar(m);
+	}
+	putchar('\n');
+
+	return (0);
+}
