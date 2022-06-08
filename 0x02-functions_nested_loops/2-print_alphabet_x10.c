85217c0821d89cb11d6e34d0eee2844fca838aac
debimpee <adebimpefolayemi1@gmail.com>
2022-06-08 14:26:01 -0700

    mile

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets in lower case 10 times
+ *
+ * Return: x10 a-z
+ */
+void print_alphabet_x10(void)
+{
+	char n, times;
+
+	times = 0;
+
+	while (times < 10)
+	{
+		for(n = 'a'; n <= 'z'; ++n)
+		{
+			_putchar(n);
+		}
+		times++;
+		_putchar('\n');
+	}
+}
