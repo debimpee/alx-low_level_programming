45074a408afbda2f03205d80a70ad70fb710be4d
debimpee <adebimpefolayemi1@gmail.com>
2022-06-09 05:25:31 -0700

    like

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,10 +21,11 @@ void times_table(void)
 
 			if (prod <= 9)
 				_putchar(' ');
-			else 
+			else
 				_putchar((prod / 10) + '0');
 
 			_putchar((prod % 10) + '0');
 		}
+		_putchar('\n');
 	}
 }
