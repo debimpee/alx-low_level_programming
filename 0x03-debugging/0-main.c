f05d8efad860443e43cd4c48aa441c5fe716d72c
debimpee <adebimpefolayemi1@gmail.com>
2022-06-10 06:11:40 -0700

    bug fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,6 +2,7 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
+ *
  * Return: 0
  */
 
@@ -11,7 +12,7 @@ int main(void)
 	int i;
 
 	i = 98;
-	positive_or_negative(i);
+	positive_or_negative();
 
 	return (0);
 }
