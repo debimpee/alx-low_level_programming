fabb5fdedb96a912c228738a4a27c479481f2e84
debimpee <adebimpefolayemi1@gmail.com>
2022-06-15 01:58:21 -0700

    prints diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,9 +15,11 @@ void print_diagonal(int n)
 			for (space = 0; space < n; space++)
 				_putchar(' ');
 			_putchar('\\');
+
+			if (len == n - 1)
+				continue;
 			_putchar('\n');
 		}
 	}
-	else
 		_putchar('\n');
 }
