5d42f16548dad6e823309379d8ce51d28c37e80a
debimpee <adebimpefolayemi1@gmail.com>
2022-06-15 04:14:40 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,14 +11,17 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if ((num % 3) != 0 && (num % 5) != 0)
-			printf("%d ", num);
-		if ((num % 3) == 0)
-			printf("Fizz ");
-		if ((num % 5) == 0)
-			printf("Buzz ");
 		if ((num % 3) == 0 && (num % 5) == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
+		else if ((num % 3) == 0)
+			printf("Fizz");
+		else if ((num % 5) == 0)
+			printf("Buzz");
+		else
+			printf("%d", num);
+		if (num == 100)
+			continue;
+		printf(" ");
 	}
 	printf("\n");
 	return (0);
