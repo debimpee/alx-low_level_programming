074634ecf191b1da87bc9db3e36b3a1eae161295
debimpee <adebimpefolayemi1@gmail.com>
2022-06-16 07:27:23 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,12 +3,17 @@
 /**
  * *_strcat - concatenates two strings.
  * @dest: the string that appears first
- * @src: the strings that comes after char *dest
+ * @src: the strings that comes after @dest
  *
- * Return: result
+ * Return: A pointer to the destination string @dest
  */
 char *_strcat(char *dest, char *src)
 {
-	_putchar(*dest);
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 	return (dest);
 }
