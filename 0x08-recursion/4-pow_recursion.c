0956ce848cc417262b919fb7dddb10bf99f13d93
debimpee <adebimpefolayemi1@gmail.com>
2022-06-22 14:52:48 -0700

    raise to power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,8 +2,8 @@
 
 /**
  * _pow_recursion - Returns the value of x raised to the power of y
- * @x: a number
- * @y: a number
+ * @x: The number to be raised
+ * @y: The power
  *
  * Return: value of x raised to the power of y if y > 0
  *         -1 if y < 0
@@ -14,5 +14,9 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	return (_pow_recursion(x, y));
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
 }
