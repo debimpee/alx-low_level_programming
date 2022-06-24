55820bc6a3b2ca3b5c17c28b9c4d2bc3fabc7546
debimpee <adebimpefolayemi1@gmail.com>
2022-06-24 03:10:35 -0700

    prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * is_prime_number - prints prime number
+ * @n: the number.
+ *
+ * Return: 1 if @n is a prime number
+ *         0 if otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n / n == 1 && n / 1 == n)
+		return (1);
+	else
+		return (0);
+}
