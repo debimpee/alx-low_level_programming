ca4dee6a0fbdad2f2b7cf789e72463a035f4d9b8
debimpee <adebimpefolayemi1@gmail.com>
2022-07-08 07:27:38 -0700

    completed tasks 0-3

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,5 +11,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	if (array == NULL || action == NULL)
 		return;
-	action(array);
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
+	}
 }
